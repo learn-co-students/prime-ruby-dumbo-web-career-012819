ffb1519a30e4bba5fabae059e48c502952971a53
andy reyes <11710reyes@gmail.com>
2018-12-28 03:51:33 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,5 +1,11 @@
-def prime?(int)
-  require 'prime'
-  Prime.prime?(int)
-  return true
+
+
+def prime?(number)
+  if number >= 2
+    (2..number - 1).all? do |x|
+      number % x != 0
+    end
+  else
+    return false
+  end
 end
